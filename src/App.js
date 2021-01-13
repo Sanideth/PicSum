c817f05a842e7b97164bacb43f489ef1a2cc173a
Sandro Bochorishvili <Sanideth@gmail.com>
2021-01-13 11:47:09 +0400

    fixed folder names and initialized React Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Header from './components/Header';
-import './App.css';
-
+import Header from "./components/Header";
+import { Switch, Route } from "react-router-dom";
+import Photos from "./pages/Photos/Photos";
+import Cart from "./pages/Cart/Cart";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
   );
 }
